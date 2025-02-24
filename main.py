dcbd0f8dcc19f55d4adc9b708464717562e39a5a
oleg <oleg@botov.org>
2025-02-24 17:04:35 +0300

    Задача 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt6 import uic
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
 import requests
@@ -13,6 +14,19 @@ class MyWidget(QMainWindow):
         self.ll = [44.269759, 46.307743]
         self.api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
         self.show_map()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp:
+            self.zoom += 1
+        elif event.key() == Qt.Key.Key_PageDown:
+            self.zoom -= 1
+        if self.zoom < 0:
+            self.zoom = 0
+        if self.zoom > 21:
+            self.zoom = 21
+        print(f'zoom={self.zoom}')
+        self.show_map()
+
 
     def show_map(self):
         api_server = "https://static-maps.yandex.ru/v1"
